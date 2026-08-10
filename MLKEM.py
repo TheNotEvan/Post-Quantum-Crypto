52085149683bd791fa4c5b7fb194886ac5f74b24
TheNotEvan <homeperlovich@gmail.com>
2026-08-09 20:20:00 -0700

    Replace polynomial multiplication with NTT in MLKEM

--- MLKEM.py
+++ MLKEM.py
@@ -32,36 +32,74 @@ class XOF:
         self._pos = end
         return out
         
-#Polynomial Arithmetic
+
+ZETA = 17
+
+def bitrev7(i):
+    return int(format(i, "07b")[::-1], 2)
+
+ZETAS = [pow(ZETA, bitrev7(i), q) for i in range(128)]
+GAMMAS = [pow(ZETA, (2 * bitrev7(i)) + 1, q) for i in range(128)]
 
 def poly_add(a, b):
     return (a + b) % q
 
-def poly_reduce(c):
-    c = c.copy()
-    for i in range(len(c) - 1, n-1, -1):
-        c[i-n] -= c[i]
-    
-    return c[:n]
+def ntt(f):
+    fhat = np.asarray(f, dtype=np.int64) % q
+    i = 1
+    length = 128
+    while length >= 2:
+        for start in range(0, n, 2 * length):
+            zeta = ZETAS[i]
+            i += 1
+            for j in range(start, start + length):
+                t = (zeta * fhat[j + length]) % q
+                fhat[j + length] = (fhat[j] - t) % q
+                fhat[j] = (fhat[j] + t) % q
+        length //= 2
+    return fhat
 
-def poly_mul(a ,b):
-    a = np.asarray(a, dtype=np.int64)
-    b = np.asarray(b, dtype=np.int64)
-    return poly_reduce(np.convolve(a, b)) % q
+def ntt_inv(fhat):
+    f = np.asarray(fhat, dtype=np.int64) % q
+    i = 127
+    length = 2
+    while length <= 128:
+        for start in range(0, n, 2 * length):
+            zeta = ZETAS[i]
+            i -= 1
+            for j in range(start, start + length):
+                t = f[j]
+                f[j] = (t + f[j + length]) % q
+                f[j + length] = (zeta * (f[j + length] - t)) % q
+        length *= 2
+    return (f * 3303) % q      
 
-def mat_vec_mul(M, v):
+def base_case_multiply(a0, a1, b0, b1, gamma):
+    c0 = ((a0 * b0) + (a1 * b1 * gamma)) % q
+    c1 = ((a0 * b1) + (a1 * b0)) % q
+    return c0, c1
+
+def multiply_ntts(fhat, ghat):
+    fhat = np.asarray(fhat, dtype=np.int64)
+    ghat = np.asarray(ghat, dtype=np.int64)
+    hhat = np.zeros(n, dtype=np.int64)
+    for i in range(128):
+        hhat[2*i], hhat[(2*i) + 1] = base_case_multiply(fhat[2*i], fhat[(2*i) + 1], ghat[2*i], ghat[(2*i) + 1], GAMMAS[i])
+    return hhat
+
+def matrix_vector_ntt(M, v):
     out = np.zeros((k,n), dtype=np.int64)
     for i in range(k):
         acc = np.zeros(n, dtype = np.int64)
         for j in range(k):
-            acc = poly_add(acc, poly_mul(M[i,j], v[j]))
+            acc = poly_add(acc, multiply_ntts(M[i,j], v[j]))
         out[i] = acc
     return out
 
-def vec_dot(u,v):
+def vec_dot_ntt(u,v):
     acc = np.zeros(n, dtype=np.int64)
     for i in range(k):
-        acc = poly_add(acc, poly_mul(u[i], v[i]))
+        acc = poly_add(acc, multiply_ntts(u[i], v[i]))
     return acc
 
 
@@ -156,7 +194,11 @@ def key_gen(d):
         E[i] = sample_poly_CBD(hashlib.shake_256(sigma + bytes([N])).digest(eta1 * 64))
         N += 1
 
-    t  = mat_vec_mul(A, S)
+    for i in range(k):
+        S[i] = ntt(S[i])
+        E[i] = ntt(E[i])
+
+    t  = matrix_vector_ntt(A, S)
     for i in range(k):
         t[i] = poly_add(t[i], E[i])
 
@@ -185,12 +227,17 @@ def encrypt(epke, m, r):
         e1[i] = sample_poly_CBD(hashlib.shake_256(r + bytes([N])).digest(eta1 * 64))
         N += 1
     e2 = sample_poly_CBD(hashlib.shake_256(r + bytes([N])).digest(eta1 * 64))
-    AT = np.transpose(A, (1,0,2))
-    u = mat_vec_mul(AT, y)
+
     for i in range(k):
-        u[i] = poly_add(u[i], e1[i])
+        y[i] = ntt(y[i])
+
+    AT = np.transpose(A, (1,0,2))
+    uhat = matrix_vector_ntt(AT, y)
+    u = np.zeros((k,n), dtype=np.int64)
+    for i in range(k):
+        u[i] = poly_add(ntt_inv(uhat[i]), e1[i])
     mu = decompress(byte_decode(m, 1), 1)
-    v = (vec_dot(t, y) + e2 + mu) % q
+    v = (ntt_inv(vec_dot_ntt(t, y)) + e2 + mu) % q
     c1 = b''.join(byte_encode(compress(u[i], du), du) for i in range(k))
     c2 = byte_encode(compress(v, dv), dv)
     return c1 + c2
@@ -199,11 +246,12 @@ def decrypt(dpke, c):
     c1 = c[0: 32 * du * k]
     c2 = c[32 * du * k: 32 * (k * du + dv)]
 
-    u = [decompress(byte_decode(c1[i * 32 * du: (i + 1) * 32 * du], du), du) for i in range(k)] 
+    u = [decompress(byte_decode(c1[i * 32 * du: (i + 1) * 32 * du], du), du) for i in range(k)]
     v = decompress(byte_decode(c2, dv), dv)
     s = [  byte_decode( dpke[i * 384: (i+1) * 384] , 12)     for i in range (k)]
+    uhat = [ntt(u[i]) for i in range(k)]
 
-    w = (v - vec_dot(u, s) ) % q
+    w = (v - ntt_inv(vec_dot_ntt(uhat, s)) ) % q
     m = byte_encode(compress(w, 1), 1)
 
     return m 
